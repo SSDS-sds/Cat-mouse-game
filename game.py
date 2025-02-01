0eb8e1207bb3f9e7b3b67c095082ba519e6cf379
SSDS-sds <warwicklee1@gmail.com>
2025-02-01 18:05:21 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ WIDTH = 600
 HEIGHT = 500
 TITLE = "Cat Game."
 
+game_over = False
 score = 0
 
 cat = Actor("cat")
@@ -17,5 +18,36 @@ def draw():
     cat.draw()
     rat.draw()
     screen.draw.text("score:"+ str(score), color = "red", fontsize = 30, topleft = (10,10))
+
+    if game_over:
+        screen.fill("red")
+        screen.draw.text("YOUR TIME IS OVER.", color = "white", fontsize = 50, midtop = (WIDTH/2,10))
+        screen.draw.text(f"Your score is {score}", color = "white", fontsize = 35, midtop = (WIDTH/2,40))
+
+
+def timer_is_up():
+    global game_over
+    game_over = True
+
+def move_cat():
+    rat.x = randint(0,600)
+    rat.y = randint(0,500)
+
+def update():
+    global score
+    if keyboard.left:
+        cat.x = cat.x - 5
+    if keyboard.right:
+        cat.x = cat.x + 5
+    if keyboard.up:
+        cat.y = cat.y - 5
+    if keyboard.down:
+        cat.y = cat.y + 5
+
+    if cat.colliderect(rat):
+        move_cat()
+        score = score + 10
+
+clock.schedule(timer_is_up, 20)
 
 pgzrun.go()
